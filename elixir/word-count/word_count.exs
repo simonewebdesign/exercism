b37d238950922c36730574a00223b2c25e905002
Simone Vittori <hello@simonewebdesign.it>
2017-03-24 11:49:39 +0000

    Make it better

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -1,19 +1,12 @@
 defmodule Words do
-  @doc """
-  Count the number of words in the sentence.
 
-  Words are compared case-insensitively.
-  """
-  @spec count(String.t) :: map
   def count(sentence) do
     sentence
-    |> String.downcase()
-    |> String.replace(~r/[^-a-z0-9]+/, " ")
-    |> String.split()
-    |> Enum.reduce(%{}, fn
-      word, acc ->
-        acc
-        |> Map.update(word, 1, & &1 + 1)
+    |> String.downcase
+    |> String.replace(~r/_|[^\w-]/u, " ")
+    |> String.split
+    |> Enum.reduce(%{}, fn word, acc ->
+      Map.update acc, word, 1, & &1 + 1
     end)
   end
 end
