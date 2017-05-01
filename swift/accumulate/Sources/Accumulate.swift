24986a4ed19b5cdcc8e0faae8f2101f742b37d32
Simone Vittori <hello@simonewebdesign.it>
2017-05-01 18:23:14 +0100

    No need for self

--- swift/accumulate/Sources/Accumulate.swift
+++ swift/accumulate/Sources/Accumulate.swift
@@ -1,6 +1,6 @@
 extension Array {
     func accumulate<T>(_ fun: (Element) -> T) -> [T] {
-        guard let head = self.first else { return [] }
-        return [fun(head)] + Array(self.dropFirst(1)).accumulate(fun)
+        guard let head = first else { return [] }
+        return [fun(head)] + Array(dropFirst(1)).accumulate(fun)
     }
 }
