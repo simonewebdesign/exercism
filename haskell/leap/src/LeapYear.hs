4cb43d56cf73356566b22146c8cb1fc4c4c74027
Simone Vittori <hello@simonewebdesign.it>
2019-07-06 19:08:12 +0100

    Add haskell/leap

---
+++ haskell/leap/src/LeapYear.hs
@@ -0,0 +1,10 @@
+module LeapYear (isLeapYear) where
+
+isLeapYear :: Integer -> Bool
+isLeapYear year =
+    if rem year 4 == 0
+        then
+            not (rem year 100 == 0)
+            || rem year 400 == 0
+        else
+            False
