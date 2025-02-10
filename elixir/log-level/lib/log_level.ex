1ea0403e1f3a0b2d70c2b7cdafa8ef9c31219b7b
Simone <hello@simonewebdesign.it>
2025-02-10 21:57:51 +0000

    Add elixir/log-level

---
+++ elixir/log-level/lib/log_level.ex
@@ -0,0 +1,52 @@
+defmodule LogLevel do
+  @spec to_label(number(), boolean()) ::
+          :debug | :error | :fatal | :info | :trace | :unknown | :warning
+  def to_label(level, legacy?) do
+    cond do
+      level == 0 and legacy? ->
+        :unknown
+
+      level == 0 ->
+        :trace
+
+      level == 1 ->
+        :debug
+
+      level == 2 ->
+        :info
+
+      level == 3 ->
+        :warning
+
+      level == 4 ->
+        :error
+
+      level == 5 and legacy? ->
+        :unknown
+
+      level == 5 ->
+        :fatal
+
+      true ->
+        :unknown
+    end
+  end
+
+  def alert_recipient(level, legacy?) do
+    label = LogLevel.to_label(level, legacy?)
+
+    cond do
+      label == :error or label == :fatal ->
+        :ops
+
+      label == :unknown and legacy? ->
+        :dev1
+
+      label == :unknown ->
+        :dev2
+
+      true ->
+        false
+    end
+  end
+end
