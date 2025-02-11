03ad367244808045dbe75a7be5b8941d397f32ed
Simone <hello@simonewebdesign.it>
2025-02-11 18:09:44 +0000

    Add dialyxir and fix spec given success typing

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -7,16 +7,16 @@ defmodule FreelancerRates do
     before_discount - before_discount * discount / 100.0
   end
 
-  @spec monthly_rate(number(), number()) :: float()
+  @spec monthly_rate(number(), number()) :: integer()
   def monthly_rate(hourly_rate, discount) do
-    hourly_rate * 8 * 22
+    (hourly_rate * 8 * 22)
     |> apply_discount(discount)
     |> ceil()
   end
 
   @spec days_in_budget(number(), number(), number()) :: float()
   def days_in_budget(budget, hourly_rate, discount) do
-    budget / apply_discount(daily_rate(hourly_rate), discount)
+    (budget / apply_discount(daily_rate(hourly_rate), discount))
     |> Float.floor(1)
   end
 end
--- elixir/freelancer-rates/mix.exs
+++ elixir/freelancer-rates/mix.exs
@@ -21,6 +21,7 @@ defmodule FreelancerRates.MixProject do
   defp deps do
     [
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
