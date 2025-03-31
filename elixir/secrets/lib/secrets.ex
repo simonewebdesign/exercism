f42ef27e347f675c897776c5902d5b9ad579e8c8
Simone <hello@simonewebdesign.it>
2025-03-31 12:23:11 +0100

    Add exercise

---
+++ elixir/secrets/lib/secrets.ex
@@ -0,0 +1,32 @@
+defmodule Secrets do
+  def secret_add(secret) do
+    fn x -> x + secret end
+  end
+
+  def secret_subtract(secret) do
+    fn x -> x - secret end
+  end
+
+  def secret_multiply(secret) do
+    fn x -> x * secret end
+  end
+
+  def secret_divide(secret) do
+    fn x -> x / secret end
+  end
+
+  def secret_and(secret) do
+    fn x -> x && secret end
+  end
+
+  def secret_xor(secret) do
+    fn x -> x || secret end
+  end
+
+  def secret_combine(secret_function1, secret_function2) do
+    fn x ->
+      secret_function1.(x)
+      secret_function2.(x)
+    end
+  end
+end
