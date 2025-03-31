607ad96ea87308abfde3247b7c3183d1a86d7f0f
Simone <hello@simonewebdesign.it>
2025-03-31 19:30:37 +0100

    Refactor

--- elixir/secrets/lib/secrets.ex
+++ elixir/secrets/lib/secrets.ex
@@ -1,33 +1,12 @@
 defmodule Secrets do
-  def secret_add(secret) do
-    fn x -> x + secret end
-  end
-
-  def secret_subtract(secret) do
-    fn x -> x - secret end
-  end
-
-  def secret_multiply(secret) do
-    fn x -> x * secret end
-  end
-
-  def secret_divide(secret) do
-    fn x -> div(x, secret) end
-  end
-
-  def secret_and(secret) do
-    fn x -> Bitwise.band(x, secret) end
-  end
-
-  def secret_xor(secret) do
-    fn x -> Bitwise.bxor(x, secret) end
-  end
+  def secret_add(secret), do: &(&1 + secret)
+  def secret_subtract(secret), do: &(&1 - secret)
+  def secret_multiply(secret), do: &(&1 * secret)
+  def secret_divide(secret), do: &div(&1, secret)
+  def secret_and(secret), do: &Bitwise.band(&1, secret)
+  def secret_xor(secret), do: &Bitwise.bxor(&1, secret)
 
   def secret_combine(secret_function1, secret_function2) do
-    fn x ->
-      x
-      |> secret_function1.()
-      |> secret_function2.()
-    end
+    &secret_function2.(secret_function1.(&1))
   end
 end
