0152aa456a627ab39852ee4633d2f8a6285041ca
Simone <hello@simonewebdesign.it>
2025-03-31 19:34:51 +0100

    Remove parens

--- elixir/secrets/lib/secrets.ex
+++ elixir/secrets/lib/secrets.ex
@@ -1,10 +1,10 @@
 defmodule Secrets do
-  def secret_add(secret), do: &(&1 + secret)
-  def secret_subtract(secret), do: &(&1 - secret)
-  def secret_multiply(secret), do: &(&1 * secret)
-  def secret_divide(secret), do: &div(&1, secret)
-  def secret_and(secret), do: &Bitwise.band(&1, secret)
-  def secret_xor(secret), do: &Bitwise.bxor(&1, secret)
+  def secret_add(secret), do: & &1 + secret
+  def secret_subtract(secret), do: & &1 - secret
+  def secret_multiply(secret), do: & &1 * secret
+  def secret_divide(secret), do: &div &1, secret
+  def secret_and(secret), do: &Bitwise.band &1, secret
+  def secret_xor(secret), do: &Bitwise.bxor &1, secret
 
   def secret_combine(secret_function1, secret_function2) do
     &secret_function2.(secret_function1.(&1))
