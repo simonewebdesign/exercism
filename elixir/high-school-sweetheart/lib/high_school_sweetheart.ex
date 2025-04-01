5e804f897f92e51b09311c5cdbbb1cb8506ee173
Simone <hello@simonewebdesign.it>
2025-04-01 21:24:40 +0100

    Add exercise

---
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -0,0 +1,27 @@
+import String
+
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    name |> trim |> first
+  end
+
+  def initial(name) do
+    n = name |> first_letter |> upcase
+    n <> "."
+  end
+
+  def initials(full_name) do
+    full_name                 # "John Doe"
+    |> split                  # ["John", "Doe"]
+    |> Enum.map(&initial(&1)) # ["J.", "D."]
+    |> Enum.join(" ")         # "J. D."
+  end
+
+  def pair(full_name1, full_name2) do
+    """
+    ❤-------------------❤
+    |  #{initials full_name1}  +  #{initials full_name2}  |
+    ❤-------------------❤
+    """
+  end
+end
