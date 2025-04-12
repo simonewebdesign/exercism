0133d38a8f50f102813221bc8b1cbb7fba4c6dda
Simone <hello@simonewebdesign.it>
2025-04-12 11:41:24 +0100

    Refactoring

--- ruby/lasagna/lasagna.rb
+++ ruby/lasagna/lasagna.rb
@@ -1,12 +1,13 @@
 class Lasagna
   EXPECTED_MINUTES_IN_OVEN = 40
+  LAYERS_PREPARATION_TIME = 2
 
   def remaining_minutes_in_oven(actual_minutes_in_oven)
     EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
   end
 
   def preparation_time_in_minutes(layers)
-    layers * 2
+    layers * LAYERS_PREPARATION_TIME
   end
 
   def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
